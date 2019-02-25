499e6e8899f632c6c6b2c36b2d33af9b13d25daf
Dubravko <woose@web.de>
2019-02-25 14:29:58 +0100

    prop definition and a further method

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,19 +2,35 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User Name: {{switchName()}}</p>
+    <p>User Name: {{myName}}</p>
+    <p>
+      <button @click="switchName">Switch Name</button>
+    </p>
+    <p>
+      <button @click="resetName">Reset Name</button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["myName"],
+  // props: ["myName"],
+  props: {
+    myName: {
+      type: String,
+      required: true
+      // default:"Daniel"
+    }
+  },
   methods: {
     switchName() {
-      return this.myName // greift auf das prop element zu
+      this.myName = this.myName // greift auf das prop element zu
         .split("")
         .reverse()
         .join("");
+    },
+    resetName() {
+      this.myName = "Alo";
     }
   }
 };
